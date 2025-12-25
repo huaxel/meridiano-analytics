538058ac5fb96c2a9b1f8691cb49177a06f33303
Juan Benjumea <benjumeamoreno@gmail.com>
2025-12-25 10:16:10 +0100

    layout push

--- reports/pages/+layout.svelte
+++ reports/pages/+layout.svelte
@@ -2,9 +2,10 @@
     import "@evidence-dev/tailwind/fonts.css";
     import "../app.css";
     import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
+    import { base } from "$app/paths";
     export let data;
 </script>
 
-<EvidenceDefaultLayout {data} logo="/meridiano.jpg">
+<EvidenceDefaultLayout {data} logo="{base}/meridiano.jpg">
     <slot slot="content" />
 </EvidenceDefaultLayout>
